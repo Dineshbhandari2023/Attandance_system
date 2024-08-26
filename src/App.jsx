70c10837953dff13c41aab3d7b4c00189d2d0fdc
Dineshbhandari2023 <iamdinesh.github@gmail.com>
2024-08-26 23:02:07 +0545

    Admin signup page completed with google Oauth verification

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import SignIn from "./components/sign-in/SignIn";
+import Signup from "./components/Admin/sign-up/SignUp";
+
+const App = () => {
+  return (
+    <div>
+      <Signup />
+      {/* <SignIn /> */}
+    </div>
+  );
+};
+
+export default App;
